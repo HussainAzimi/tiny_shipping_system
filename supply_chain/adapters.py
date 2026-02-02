14abaa1e5d956b0554d875b12d9ea33347f5f4cc
CAPS-EmpowerELP <AD-CAPS-EmpowerELP@email.wustl.edu>
2026-02-01 22:00:59 -0600

    file adapters.py updated

--- supply_chain/adapters.py
+++ supply_chain/adapters.py
@@ -9,9 +9,22 @@ from .warehouse import Warehouse
 def to_csv(warehouse: Warehouse) -> str:
     """Return CSV text for the warehouse using its `to_rows()` method."""
     # TODO: implement using csv.DictWriter to write to an in-memory buffer
-    raise NotImplementedError
+    rows = warehouse.to_rows()
+
+    if not rows:
+        return ""
+    
+    output = io.StringIO()
+    fieldname = rows[0].keys()
+
+    writer = csv.DictWriter(output, fieldnames=fieldname)
+    writer.writeheader
+    writer.writerows
+
+    return output.getvalue()
 
 def to_json(warehouse: Warehouse) -> str:
     """Return JSON string for the warehouse using its `to_rows()` method."""
-    # TODO: implement using json.dumps
-    raise NotImplementedError
+    rowws = warehouse.to_rows()
+
+    return json.dumps(rowws, indent=4, sort_keys=True)
